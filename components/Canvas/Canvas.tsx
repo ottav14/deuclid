5b40c9bfc9b29a88cb695b2c598c3ba10299422e
dom <dominicjottaviano@gmail.com>
2024-11-20 14:53:25 -0500

    Started line tool.

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -5,15 +5,18 @@ import Button from "../Button/Button.tsx";
 import circleIcon from "../../public/icons/circle.svg";
 import lineIcon from "../../public/icons/line.svg";
 import { useRef, useState, useEffect } from 'react';
-import { circle, point, drawScreen } from "./draw.tsx";
+import { drawScreen } from "./draw.tsx";
 
 const Canvas = () => {
 	const canvasRef = useRef(null);
 	const [isDrawing, setIsDrawing] = useState(false);
 	const [currentMode, setMode] = useState('circle');
 	const [currentCircle, setCurrentCircle] = useState(null);
+	const [currentPoint, setCurrentPoint] = useState(null);
+	const [currentLine, setCurrentLine] = useState(null);
 	const [circles, setCircles] = useState([]);
 	const [points, setPoints] = useState([]);
+	const [lines, setLines] = useState([]);
 	const ctxRef = useRef(null);
 	const circleButtonRef = useRef();
 	const lineButtonRef = useRef();
@@ -22,9 +25,14 @@ const Canvas = () => {
 	   	"line": lineButtonRef
 	};
 
+	const drawLoop = () => {
+		drawScreen(circles, points, lines, currentCircle, currentPoint, currentLine, canvasRef.current);
+	}
+
 	const point = (x, y) => { 
 		const p = { x: x, y: y };
 		setPoints(previousPoints => [...previousPoints, p]);
+		setCurrentPoint(points.length);
 
 		return p;
 	}
@@ -38,12 +46,14 @@ const Canvas = () => {
 		return c;
 	}
 
-	useEffect(() => {
-		// Get the canvas context once the component is mounted
-		const canvas = canvasRef.current;
-		const ctx = canvas.getContext('2d');
+	const line = (x1, y1, x2, y2) => { 
 
-	}, []);
+		const l = { x1: x1, y1: y1, x2: x2, y2: y2 };
+		setLines(previousLines => [...previousLines, l]);
+		setCurrentLine(lines.length);
+
+		return l;
+	}
 
 	const screenToCanvas = (e) => {
 		const canvas = canvasRef.current;
@@ -62,8 +72,16 @@ const Canvas = () => {
 			
 		if(!isDrawing) {
 			const pos = screenToCanvas(e);
-			circle(pos.x, pos.y, 30);
-			point(pos.x, pos.y);
+			switch(currentMode) {
+				case "circle":
+					circle(pos.x, pos.y, 30);
+					point(pos.x, pos.y);
+					break;
+				case "line":
+					line(pos.x, pos.y, pos.x, pos.y);
+					point(pos.x, pos.y);
+					break;
+			}
 		}
 
 	}
@@ -73,16 +91,29 @@ const Canvas = () => {
 	}
 
 	const mouseMove = (e) => {
-
-		if(isDrawing && currentMode == "circle") {
-			let newCircles = circles;
-			let c = circles[currentCircle];
+		if(isDrawing) {
 			let pos = screenToCanvas(e);
-			let d = distance(pos.x, pos.y, c.x, c.y);
-			c.r = d;
-			newCircles[currentCircle] = c;
-			setCircles(newCircles);
-			drawScreen(circles, points, canvasRef.current);
+			switch(currentMode) {
+				case "circle":
+					let newCircles = circles;
+					let c = circles[currentCircle];
+					let d = distance(pos.x, pos.y, c.x, c.y);
+					c.r = d;
+					newCircles[currentCircle] = c;
+					setCircles(newCircles);
+					break;
+				case "line":
+					let newLines = lines;
+					let l = lines[currentLine];
+					l.x2 = pos.x;
+					l.y2 = pos.y;
+					/*
+					newLines[currentLine] = l;
+					setLines(newLines);
+				   */
+					break;
+			}
+			drawLoop();
 		}
 
 	}
@@ -92,17 +123,24 @@ const Canvas = () => {
 		const currentRef = buttonRefs[currentMode];
 		currentRef.current.setState(false);
 		setMode("circle");
+		setIsDrawing(false);
 
 	}
-
 
 	const enterLineMode = () => {
 		
 		const currentRef = buttonRefs[currentMode];
 		currentRef.current.setState(false);
 		setMode("line");
+		setIsDrawing(false);
 
 	}
+
+	useEffect(() => {
+
+		drawLoop();
+
+	}, []);
 
 	return (
 		<div className={styles.main}>
--- components/Canvas/draw.tsx
+++ components/Canvas/draw.tsx
@@ -1,14 +1,9 @@
-export const drawBackground = (canvas) => {
-	const ctx = canvas.getContext('2d');
-
+const drawBackground = (canvas, ctx) => {
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 }
 
-export const drawCircle = (circle, canvas) => {
-	const ctx = canvas.getContext('2d');
-
+const drawCircle = (circle, ctx) => {
 	ctx.strokeStyle = "blue";
 	ctx.beginPath(); 
 	ctx.arc(circle.x, circle.y, circle.r, 0, 2*Math.PI);
@@ -16,15 +11,7 @@ export const drawCircle = (circle, canvas) => {
 	ctx.closePath();
 }
 
-export const drawCircles = (circles, canvas) => {
-	for(let i=0; i<circles.length; i++) {
-		drawCircle(circles[i], canvas);
-	}
-}
-
-export const drawPoint = (point, canvas) => {
-	const ctx = canvas.getContext('2d');
-
+const drawPoint = (point, ctx) => {
 	ctx.fillStyle = "blue";
 	ctx.beginPath(); 
 	ctx.arc(point.x, point.y, 3, 0, 2*Math.PI);
@@ -32,14 +19,37 @@ export const drawPoint = (point, canvas) => {
 	ctx.closePath();
 }
 
-export const drawPoints = (points, canvas) => {
-	for(let i=0; i<points.length; i++) {
-		drawPoint(points[i], canvas);
+const drawLine = (line, ctx) => {
+	ctx.strokeStyle = "blue";
+	ctx.beginPath(); 
+	ctx.moveTo(line.x1, line.y1);
+	ctx.lineTo(line.x2, line.y2);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+const drawCircles = (circles, currentCircle, ctx) => {
+	for(let i=0; i<circles.length; i++) {
+		drawCircle(circles[i], ctx);
 	}
 }
 
-export const drawScreen = (circles, points, canvas) => {
-	drawBackground(canvas);
-	drawCircles(circles, canvas);
-	drawPoints(points, canvas);
+const drawPoints = (points, currentPoint, ctx) => {
+	for(let i=0; i<points.length; i++) {
+		drawPoint(points[i], ctx);
+	}
+}
+
+const drawLines = (lines, currentLine, ctx) => {
+	for(let i=0; i<lines.length; i++) {
+		drawLine(lines[i], ctx);
+	}
+}
+
+export const drawScreen = (circles, points, lines, currentCircle, currentPoint, currentLine, canvas) => {
+	const ctx = canvas.getContext('2d');
+	drawBackground(canvas, ctx);
+	drawCircles(circles, currentCircle, ctx);
+	drawPoints(points, currentPoint, ctx);
+	drawLines(lines, currentLine, ctx);
 }
